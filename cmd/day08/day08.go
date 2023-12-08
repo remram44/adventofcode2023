543af0fd95c734983f7a683d610cdc4cc32b4c39
Remi Rampin <remi@rampin.org>
2023-12-08 10:17:09 -0500

    Day 08 part 2

--- cmd/day08/day08.go
+++ cmd/day08/day08.go
@@ -53,7 +53,7 @@ func main() {
 		network[fromNode] = [2]string{toNodeL, toNodeR}
 	}
 
-	// Follow the directions
+	// Follow the directions in part 1
 	steps := 0
 	currentNode := "AAA"
 	for currentNode != "ZZZ" {
@@ -63,4 +63,69 @@ func main() {
 	}
 
 	fmt.Println(steps)
+
+	// Find starting nodes for part 2
+	var currentNodes []string
+	for node := range network {
+		if node[2] == 'A' {
+			currentNodes = append(currentNodes, node)
+		}
+	}
+	log.Println(currentNodes)
+
+	// Follow the directions in part 2
+	// Stop when each concurrent path has reached an exit *once*
+	steps = 0
+	reachedZ := make([]int, len(currentNodes))
+	for i := 0; i < len(currentNodes); i += 1 {
+		reachedZ[i] = -1
+	}
+	for {
+		allDone := true
+		for i, node := range currentNodes {
+			reached := reachedZ[i]
+			if reached == -1 {
+				if node[2] == 'Z' {
+					log.Printf("starting node %v reached %v after %v steps", i, node, steps)
+					reachedZ[i] = steps
+				} else {
+					allDone = false
+				}
+			}
+		}
+
+		if allDone {
+			break
+		}
+
+		// Follow directions
+		direction := directions[steps%len(directions)]
+		steps += 1
+		for i := range currentNodes {
+			currentNodes[i] = network[currentNodes[i]][direction]
+		}
+	}
+
+	// Now find the least common multiple of those numbers
+	fmt.Println(leastCommonMultiple(reachedZ))
+}
+
+func leastCommonMultiple(numbers []int) int {
+	result := numbers[0]
+
+	for i := 1; i < len(numbers); i += 1 {
+		result = result * numbers[i] / greatestCommonDivisor(result, numbers[i])
+	}
+
+	return result
+}
+
+// Euclid's
+func greatestCommonDivisor(a int, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
 }
