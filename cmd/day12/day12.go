3851bb8fed133644b70abd51335cf7bb4b49d478
Remi Rampin <remi@rampin.org>
2023-12-13 16:11:41 -0500

    Memoize

--- cmd/day12/day12.go
+++ cmd/day12/day12.go
@@ -93,9 +93,9 @@ func main() {
 
 				// Find arrangements for part 1
 				//log.Print(line)
-				arrangements1 := countArrangements(sequence, groupSizes, 0)
 				//log.Printf("%v -> %v", line, arrangements1)
 				//log.Print()
+				arrangements1 := countArrangements(sequence, groupSizes, 0, make(map[string]int))
 				atomic.AddInt64(&sumOfArrangementsPart1, int64(arrangements1))
 
 				// Unfold the sequence
@@ -119,7 +119,7 @@ func main() {
 
 				// Find arrangements for part 2
 				//log.Printf("%v x5", line)
-				arrangements2 := countArrangements(sequence2, groupSizes2, 0)
+				arrangements2 := countArrangements(sequence2, groupSizes2, 0, make(map[string]int))
 				//log.Printf("%v x5 -> %v", line, arrangements2)
 				//log.Print()
 				atomic.AddInt64(&sumOfArrangementsPart2, int64(arrangements2))
@@ -145,7 +145,7 @@ func main() {
 	fmt.Println(sumOfArrangementsPart2)
 }
 
-func countArrangements(sequence []spring, groupSizes []int, depth int) int {
+func countArrangements(sequence []spring, groupSizes []int, depth int, memo map[string]int) int {
 	//log.Printf("%vcountArrangements(%v, %v)", strings.Repeat(" ", depth), sequence, groupSizes)
 	if len(sequence) == 0 {
 		if len(groupSizes) == 0 {
@@ -160,28 +160,40 @@ func countArrangements(sequence []spring, groupSizes []int, depth int) int {
 	switch sequence[0] {
 	case Empty:
 		//log.Printf("%v empty, consume sequence", strings.Repeat(" ", depth))
-		result := countArrangements(sequence[1:], groupSizes, depth+1)
+		result := countArrangements(sequence[1:], groupSizes, depth+1, memo)
 		//log.Printf("%v return %v", strings.Repeat(" ", depth), result)
 		return result
 	case Present:
-		result := countArrangementsIfPresent(sequence, groupSizes, depth)
+		result := countArrangementsIfPresent(sequence, groupSizes, depth, memo)
 		//log.Printf("%v return %v", strings.Repeat(" ", depth), result)
 		return result
 	case Unknown:
-		total := 0
-		//log.Printf("%v try guess absent", strings.Repeat(" ", depth))
-		total += countArrangements(sequence[1:], groupSizes, depth+1)
-		//log.Printf("%v try guess present", strings.Repeat(" ", depth))
-		total += countArrangementsIfPresent(sequence, groupSizes, depth)
-		//log.Printf("%v return %v", strings.Repeat(" ", depth), total)
-		return total
+		key := ""
+		for _, e := range sequence {
+			key += fmt.Sprintf(",%d", e)
+		}
+		key += "|"
+		for _, e := range groupSizes {
+			key += fmt.Sprintf(",%d", e)
+		}
+		result, ok := memo[key]
+		if !ok {
+			result = 0
+			//log.Printf("%v try guess absent", strings.Repeat(" ", depth))
+			result += countArrangements(sequence[1:], groupSizes, depth+1, memo)
+			//log.Printf("%v try guess present", strings.Repeat(" ", depth))
+			result += countArrangementsIfPresent(sequence, groupSizes, depth, memo)
+			//log.Printf("%v return %v", strings.Repeat(" ", depth), total)
+			memo[key] = result
+		}
+		return result
 	default:
 		log.Fatal("Unknown item in sequence")
 		return 0
 	}
 }
 
-func countArrangementsIfPresent(sequence []spring, groupSizes []int, depth int) int {
+func countArrangementsIfPresent(sequence []spring, groupSizes []int, depth int, memo map[string]int) int {
 	if len(groupSizes) == 0 {
 		//log.Printf("%v can't consume present, empty group list", strings.Repeat(" ", depth))
 		return 0
@@ -211,5 +223,5 @@ func countArrangementsIfPresent(sequence []spring, groupSizes []int, depth int) 
 		return 0
 	}
 	//log.Printf("%v consume %v present 1 absent", strings.Repeat(" ", depth), groupSize)
-	return countArrangements(sequence[groupSize+1:], groupSizes[1:], depth+1)
+	return countArrangements(sequence[groupSize+1:], groupSizes[1:], depth+1, memo)
 }
